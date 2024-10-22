5eb13da834ef8447a1beab6f04fe305dd314afa8
deval <deval.patel5@dcmail.ca>
2024-10-22 02:24:17 -0400

    Initial commit

---
+++ fetch_data_by_webscrapping.py
@@ -0,0 +1,3 @@
+class FetchData():
+    def __init__(self):
+        pass
---
+++ job_and_skill_search.py
@@ -0,0 +1,6 @@
+class JobSearchWithSkills:
+    def __init__(self, job_title, size=50):
+        self.job_title = job_title
+        self.size = size
+
+
