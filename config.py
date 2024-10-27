d894b4441c8f46490b22b7cefde51e6d6cc84452
deval <deval.patel5@dcmail.ca>
2024-10-27 17:09:28 -0400

    everythong going well

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 HEADERS = {
+    'content-type': 'text/html',
+    'x-content-security-policy-nonce': 'LZiDbFRnRkUDTjyW5-kYkA',
+    'x-fs-uuid': '00062558baf8c6bc122b322944de6285',
+    'x-li-uuid': 'AAYlWLr4xrwSKzIpRN5ihQ==',
+    'sec-ch-prefers-color-scheme': 'dark',
     'accept': 'ext/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-encoding': 'gzip, deflate, br, zstd',
     'accept-language': 'en-US,en;q=0.9,hi;q=0.8,gu;q=0.7',
@@ -13,8 +18,31 @@ HEADERS = {
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+
 }
-
-BASE_URL = 'https://www.linkedin.com/jobs/search/?keywords='
-
-JOB_CLASS_CONTAINER = 'scaffold-layout__list-container'
+COOKIE = AUTHENTICATED_COOKIES = {
+    "li_sugr": "7d4895c4-83e1-4f4a-9626-d421ce925753",
+    "AnalyticsSyncHistory": "AQJZcTAeTurgfAAAAZKcPbN6VbWuQGqLrXx5E8N1B_PimjF_tyZ3K2-VaovjfEYrXlL9-jSWO8DaKek3HQwDTw",
+    "bcookie": "v=2&e20bd5fd-727d-46d6-8f26-b353baf1cc6a",
+    "bscookie": "v=1&20241017204902baf746c7-64c0-47fe-874e-43ed55b7d65aAQFxKjfubMSb6_BlOWZgflLFf2XRZGf6",
+    "timezone": "America/Toronto",
+    "dfpfpt": "2a7371fead724d48a4cf75ba00de7acb",
+    "li_rm": "AQE6A1qkSpCFdgAAAZKraljAYWa8QmMqdxqIYEUsnmW43eF98s5Au2HZQhHN80YkI_RkCCNz_6KPHj2oEqUYYONwSpWSYHUwsr4Eu6PDehxB3MXQisszcRFK",
+    "visit": "v=1&M",
+    "g_state": "{\"i_l\":1,\"i_p\":1729459939265}",
+    "li_theme_set": "user",
+    "li_theme": "dark",
+    "liap": "true",
+    "li_at": "AQEDAUoX_LMAxtDcAAABkraBTbsAAAGS2o3Ru1YAGxMi2AarecLirDM6re1Zv7ka-wAvaEA7u9I2pzN_HopP4QsaAbsDtxNGxmEFlvlaI8pW5LEJ2izb3OqjTV2XvTVydzU_P6hS56UGZhIh7lt1qqpa",
+    "JSESSIONID": "ajax:3753697237924815166",
+    "lang": "v=2&lang=en-us",
+    "fptctx2": "taBcrIH61PuCVH7eNCyH0LNKRXFdWqLJ6b8ywJyet7WmdqUjKGnwKULJH0tEgmswjg/fWkbEfPTix+KXrH3gwdZJ1YyoThrKdenkcl8GcrT8zXtS7fX1mDz97FZ4uo73DZ0MBnslH2uBE9DKZuY8zTQmVdKVyUGYQvdVtdXax2K+PDBHK1pNBWj81kIry7J8baYR8rmPsgvjxtBlcEgg/FblNMdun1Pnrkl7q/qt7tBIspy67JD2F+CjwESOUwYAm9NJN6CShaFv1SRz+qNdMx9YT32T0cXDTQpVHylw1PUNE5rK6B97u1QPl0a6E0X6UK5SxDfqjfV79cgmam8GBNPtaSSOZZcgIbkVB1Eikam0=",
+    "lidc": "b=OB03:s=O:r=O:a=O:p=O:g=6553:u=36:x=1:i=1729907173:t=1729975182:v=2:sig=AQEf5jdJvCwiDl65Axs1aoUCkjv3P0m8",
+    "UserMatchHistory": "AQI_QuoA-2hRwQAAAZLG132Pgf8qgJbfpZGdUth5pieCaj-nLcA7tfyRtzw3VJmBGYJFGAypih2BO4vi8bZu8SaU7Xv4V0Mhko_94_b3AV3QLnSg3HteO1nUVZyYSp4AAHb4VbiHK3d9swHddb0Ajmot5ScgDqAH2CIDgJJoo5OSZP8S_TeCStWZyLvJoo_EiC7jJoUAC7XLjN6rvo7cn62qOBDMYsuluXnYx9F2LZjah3CoC6I1vD7nwkRqKLZKIXlMWEfw3GGQFfTgyqk9kLWVa-zelAU7CY98aXrpR9M0vr8L9gPvfMax1HM7GOiiyRlP8jPiDuMBjH6qCyzK42tIzt_Dn4GK4sEO_BcJwWnSBScB9w",
+    "sdsc": "22%3A1%2C1729912865947%7EJAPP%2C0m7GGwXYz3CogxHYJzSG4asq7ing="
+}
+# https://www.linkedin.com/jobs/search/?origin=JOB_SEARCH_PAGE_SEARCH_BUTTON&refresh=true
+# https://www.linkedin.com/jobs/search?trk=organization_guest_guest_nav_menu_jobs&position=1&pageNum=0&original_referer=
+BASE_URL = 'https://www.linkedin.com/jobs/search?trk=organization_guest_guest_nav_menu_jobs&position=1&pageNum=0&original_referer='
+JOB_URL = ''
+JOB_CLASS_CONTAINER = ''
--- fetch_data_by_webscrapping.py
+++ fetch_data_by_webscrapping.py
@@ -1,102 +1,36 @@
-import time
+import json
 import requests
 from bs4 import BeautifulSoup
 import config as c
-from selenium import webdriver
-import confidential
 
 
-class DataFetcher():
-    def __init__(self, user_name, password, job_title, size):
+class DataFetcher:
+    def __init__(self, job_title, size=20):
         self.job_title = job_title
         self.size = size
         self.headers = c.HEADERS
-        self.payload = c.PAYLOAD
         self.base_url = c.BASE_URL
-        self.login_url = c.LOGIN_URL
-        self.job_class_container = c.JOB_CLASS_CONTAINER
-        self.user_name = user_name
-        self.password = password
-        self.session = requests.Session()  # Use the same session for all requests
-
+        self.cookie = c.COOKIE
+        self.job_ids = []
         self.initialize()
 
     def initialize(self):
-        payload_data = self.get_payload_data(username_=self.user_name, password_=self.password,)
-        print(payload_data)
-        cookie = self.get_cookie()
-        if payload_data:
-            self.linkedin_login(payload_=payload_data, cookie_=cookie)
-
-    def get_cookie(self):
-        response = self.session.get(self.login_url)
-        # Check if the request was successful
-        if response.status_code == 200:
-            # Extract cookies from the response
-            cookies = response.cookies.get_dict()
-
-            # Convert cookies to the format required for headers
-            cookie_header = '; '.join([f'{key}={value}' for key, value in cookies.items()])
-            return cookie_header
-        else:
-            print("Failed to retrieve the login page. Status code:", response.status_code)
-            return None
-
-    def get_payload_data(self, username_,password_):
-        response = self.session.get(self.login_url)
-        # Check if the request was successful
-        if response.status_code == 200:
-            # Parse the HTML content
-            soup = BeautifulSoup(response.text, 'html.parser')
-
-            # Extract necessary tokens and parameters
-            loginCsrfParam = soup.find('input', {'name': 'loginCsrfParam'})['value']
-            csrfToken = soup.find('input', {'name': 'csrfToken'})['value']
-            sIdString = soup.find('input', {'name': 'sIdString'})['value']
-            parentPageKey = soup.find('input', {'name': 'parentPageKey'})['value']
-            pageInstance = soup.find('input', {'name': 'pageInstance'})['value']
-            apfc = soup.find('input', {'name': 'apfc'})['value']
-
-            return {
-                'session_key' : username_,
-                'session_password' : password_,
-                'loginCsrfParam': loginCsrfParam,
-                'csrfToken': csrfToken,
-                'sIdString': sIdString,
-                'parentPageKey': parentPageKey,
-                'pageInstance': pageInstance,
-                'apfc': apfc
-            }
-        else:
-            print("Failed to retrieve the login page. Status code:", response.status_code)
-            return None
-
-    def linkedin_login(self, payload_, cookie_):
-        payload = self.payload
-        headers = self.headers
-        payload.update(payload_)
-        headers.update(cookie_)
-        login_response = self.session.post(url=self.login_url, data=payload, headers=headers)
-
-        if login_response.status_code == 200 and "successful" in login_response.text.lower():
-            print("Login successful!")
-        else:
-            print("Login failed. Response:", login_response.text)
+        html_content = self.get_html()
+        self.get_job_ids(html_content=html_content)
 
     def get_html(self):
-        pass
+        url = self.base_url
+        while True:
+            response = requests.get(url=url,headers=self.headers)
+            if response.status_code == 200:
+                return response.text
 
-    def get_job_containers(self, webpage, classid):
-        pass
-
-    def get_skills(self, job_container, skillsid):
-        pass
-
-    def get_additional_details(self):
-        pass
+    def get_job_ids(self, html_content):
+        soup = BeautifulSoup(html_content, 'lxml')
+        job_search_list = soup.find('ul',class_='jobs-search__results-list')
 
 
-username = confidential.USERNAME
-password = confidential.PASSWORD
+
+
 # Example usage
-DataFetcher(user_name=username, password=password, job_title='Machine Learning', size=0)
+df = DataFetcher(job_title='python')
