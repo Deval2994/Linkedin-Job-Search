5697a4de5700516dbcfc009a44eab27ab2785cb3
deval <deval.patel5@dcmail.ca>
2024-10-28 16:34:03 -0400

    added cookie

--- config.py
+++ config.py
@@ -1,47 +1,50 @@
 HEADERS = {
-    'content-type': 'text/html',
-    'x-content-security-policy-nonce': 'LZiDbFRnRkUDTjyW5-kYkA',
-    'x-fs-uuid': '00062558baf8c6bc122b322944de6285',
-    'x-li-uuid': 'AAYlWLr4xrwSKzIpRN5ihQ==',
-    'sec-ch-prefers-color-scheme': 'dark',
-    'accept': 'ext/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'authority': 'www.linkedin.com',
+    'method': 'GET',
+    'path': '/jobs/',
+    'scheme': 'https',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-encoding': 'gzip, deflate, br, zstd',
     'accept-language': 'en-US,en;q=0.9,hi;q=0.8,gu;q=0.7',
     'cache-control': 'max-age=0',
-    'referer': 'https://www.ambitionbox.com/list-of-companies',
+    'priority': 'u=0, i',
+    'sec-ch-prefers-color-scheme': 'dark',
     'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
     'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': "Windows",
+    'sec-ch-ua-platform': '"Windows"',
     'sec-fetch-dest': 'document',
     'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'same-origin',
+    'sec-fetch-site': 'none',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
-
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
 }
-COOKIE = AUTHENTICATED_COOKIES = {
+
+COOKIE = {
     "li_sugr": "7d4895c4-83e1-4f4a-9626-d421ce925753",
     "AnalyticsSyncHistory": "AQJZcTAeTurgfAAAAZKcPbN6VbWuQGqLrXx5E8N1B_PimjF_tyZ3K2-VaovjfEYrXlL9-jSWO8DaKek3HQwDTw",
-    "bcookie": "v=2&e20bd5fd-727d-46d6-8f26-b353baf1cc6a",
-    "bscookie": "v=1&20241017204902baf746c7-64c0-47fe-874e-43ed55b7d65aAQFxKjfubMSb6_BlOWZgflLFf2XRZGf6",
     "timezone": "America/Toronto",
+    "bcookie": '"v=2&e20bd5fd-727d-46d6-8f26-b353baf1cc6a"',
+    "bscookie": '"v=1&20241017204902baf746c7-64c0-47fe-874e-43ed55b7d65aAQFxKjfubMSb6_BlOWZgflLFf2XRZGf6"',
     "dfpfpt": "2a7371fead724d48a4cf75ba00de7acb",
     "li_rm": "AQE6A1qkSpCFdgAAAZKraljAYWa8QmMqdxqIYEUsnmW43eF98s5Au2HZQhHN80YkI_RkCCNz_6KPHj2oEqUYYONwSpWSYHUwsr4Eu6PDehxB3MXQisszcRFK",
     "visit": "v=1&M",
-    "g_state": "{\"i_l\":1,\"i_p\":1729459939265}",
+    "g_state": '{"i_l":1,"i_p":1729459939265}',
     "li_theme_set": "user",
     "li_theme": "dark",
     "liap": "true",
     "li_at": "AQEDAUoX_LMAxtDcAAABkraBTbsAAAGS2o3Ru1YAGxMi2AarecLirDM6re1Zv7ka-wAvaEA7u9I2pzN_HopP4QsaAbsDtxNGxmEFlvlaI8pW5LEJ2izb3OqjTV2XvTVydzU_P6hS56UGZhIh7lt1qqpa",
-    "JSESSIONID": "ajax:3753697237924815166",
-    "lang": "v=2&lang=en-us",
-    "fptctx2": "taBcrIH61PuCVH7eNCyH0LNKRXFdWqLJ6b8ywJyet7WmdqUjKGnwKULJH0tEgmswjg/fWkbEfPTix+KXrH3gwdZJ1YyoThrKdenkcl8GcrT8zXtS7fX1mDz97FZ4uo73DZ0MBnslH2uBE9DKZuY8zTQmVdKVyUGYQvdVtdXax2K+PDBHK1pNBWj81kIry7J8baYR8rmPsgvjxtBlcEgg/FblNMdun1Pnrkl7q/qt7tBIspy67JD2F+CjwESOUwYAm9NJN6CShaFv1SRz+qNdMx9YT32T0cXDTQpVHylw1PUNE5rK6B97u1QPl0a6E0X6UK5SxDfqjfV79cgmam8GBNPtaSSOZZcgIbkVB1Eikam0=",
-    "lidc": "b=OB03:s=O:r=O:a=O:p=O:g=6553:u=36:x=1:i=1729907173:t=1729975182:v=2:sig=AQEf5jdJvCwiDl65Axs1aoUCkjv3P0m8",
-    "UserMatchHistory": "AQI_QuoA-2hRwQAAAZLG132Pgf8qgJbfpZGdUth5pieCaj-nLcA7tfyRtzw3VJmBGYJFGAypih2BO4vi8bZu8SaU7Xv4V0Mhko_94_b3AV3QLnSg3HteO1nUVZyYSp4AAHb4VbiHK3d9swHddb0Ajmot5ScgDqAH2CIDgJJoo5OSZP8S_TeCStWZyLvJoo_EiC7jJoUAC7XLjN6rvo7cn62qOBDMYsuluXnYx9F2LZjah3CoC6I1vD7nwkRqKLZKIXlMWEfw3GGQFfTgyqk9kLWVa-zelAU7CY98aXrpR9M0vr8L9gPvfMax1HM7GOiiyRlP8jPiDuMBjH6qCyzK42tIzt_Dn4GK4sEO_BcJwWnSBScB9w",
-    "sdsc": "22%3A1%2C1729912865947%7EJAPP%2C0m7GGwXYz3CogxHYJzSG4asq7ing="
+    "fptctx2": "taBcrIH61PuCVH7eNCyH0LNKRXFdWqLJ6b8ywJyet7WmdqUjKGnwKULJH0tEgmswjg%252fWkbEfPTix%252bKXrH3gwdZJ1YyoThrKdenkcl8GcrT8zXtS7fX1mDz97FZ4uo73DZ0MBnslH2uBE9DKZuY8zTSj5EqGnjbxSpLBdLrtNfnuoAnozsLVyfml%252fiC22Nd0ZDVVb9jgO9ezGznrsBHlJoecobzU4nrIRHmd%252fSVgJZ7rWHLzuqovlKe1j6FUfZjrj%252f7tnJhge%252fcWfSbzs9SkoVjr8WYHdxBnWttaE7V7b4xdWXhqHn%252fSxmZ3%252fSEMqSmQ8x6Bnbzfjy0tAFJuHeYg63%252b08y66X8Sbrd27ldqzRlvU%253d",
+    "UserMatchHistory": "AQKfkU2tQRvIqgAAAZLUMVwlfPDzp1dCtFTSlIB97yuVQzY4NTxZxxKE_3zF5cLA6alh-zZoXxvELsvO5so6sKw4HQVqQDdJe6bql4cEoV1LOD-c7WoOSJzC94Yt2REDypU_w51FtYr1SDT4TABztPKDyRIOIGn2wnAtG7qBP8ngrOOIPRets6zf3fDhDnIQfN9tcvLzdfz_l5c514ZXovjrPjSkma48wt35KYDfmnJaTTTeklYSZyTb8XY2vkCArOR8sd9t_yEXTiUsBHm2VYpRSYGv3uiRNeOwvKEbOhsA3QA7eMNkplh3pbRJXFYCkmhXCvT7ooHiwn2Ix6ldxqyFY5VoPO8p1byTGa-x0izEu7Fb5w"
 }
 
-BASE_URL = 'https://www.linkedin.com/jobs/search?trk=organization_guest_guest_nav_menu_jobs&position=1&pageNum=0&original_referer='
+
+
+# https://www.linkedin.com/jobs/search?keywords=Project%20Management&location=Canada&currentJobId=
+
+
+job_posting_url = 'https://www.linkedin.com/jobs/search?keywords='
+# https%3A%2F%2Fwww.linkedin.com%2Fjobs%2Fsearch%3Ftrk%3Dorganization_guest_guest_nav_menu_jobs%26position%3D1%26pageNum%3D0%26original_referer%3D&position=1&pageNum=0
+BASE_URL = 'https://www.linkedin.com/jobs/search?keywords='
 INFO_CONTAINER_DIV = 'base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card'
 JOB_CLASS_CONTAINER_UL = 'jobs-search__results-list'
