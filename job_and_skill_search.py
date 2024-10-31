feab081e1b7df1249992f6f4d4a33eb0bcb095df
deval <deval.patel5@dcmail.ca>
2024-10-31 01:38:55 -0400

    just changed limit to 1 for testing

--- job_and_skill_search.py
+++ job_and_skill_search.py
@@ -2,4 +2,4 @@ import confedential as c
 from fetch_data_by_webscrapping import DataFetcher
 
 if __name__ == "__main__":
-    DataFetcher(username=c.username,password=c.password,job_title='Machine Learning')
+    DataFetcher(username=c.username,password=c.password,job_title='Machine Learning',size=1)
