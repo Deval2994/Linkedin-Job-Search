e694cde2cc97d345f777545f4082d830ef8b15dc
deval <deval.patel5@dcmail.ca>
2024-11-01 01:11:46 -0400

    more configs

--- config.py
+++ config.py
@@ -9,6 +9,10 @@ UNSEEN_JOB_LI = 'ember-view jobs-search-results__job-card-search--generic-occlud
 JOB_CLASS_CONTAINER_DIV = 'jobs-search-results-list'
 JOB_ID_ENTITY = 'data-occludable-job-id'
 SKIP_JOB_ENTITY = 25
-JOB_URL = 'https://www.linkedin.com/jobs/view/'
+JOB_URL = 'https://www.linkedin.com/jobs/search/?keywords=Machine%20Learning&currentJobId='
 SKILL_LINK_SELECTOR = '#how-you-match-card-container > section:nth-child(2) > div > div.pt5 > div:nth-child(3) > div > a'
 SKILL_CONTAINER_UL_CLASS = 'job-details-skill-match-status-list'
+SKILL_TEXT_A = 'app-aware-link job-details-how-you-match__skills-item-subtitle t-14 overflow-hidden'
+JOB_POSITION_H1_CLASS = 't-24 t-bold inline'
+JOB_TYPE_SPAN_CLASS = 'ui-label ui-label--accent-3 text-body-small'
+JOB_TYPE_SPAN_CLASS_2 = 'job-details-jobs-unified-top-card__job-insight-view-model-secondary'
