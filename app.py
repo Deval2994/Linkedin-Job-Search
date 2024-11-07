83f0ccb5fc686cf82087f7b99de63dd0a428f8bb
deval <deval.patel5@dcmail.ca>
2024-11-07 02:48:02 -0500

    able to download the files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from fetch_data_by_webscrapping import DataFetcher
 from export_data import ExportData
 import confedential as c  # Assuming confedential stores your LinkedIn credentials
@@ -6,6 +6,10 @@ from flask import Flask, render_template, request
 import json
 
 app = Flask(__name__)
+
+JSON_FILE_PATH = 'data/job data.json'
+CSV_FILE_PATH = 'data/job data.csv'
+EXCEL_FILE_PATH = 'data/job data.xlsx'
 
 
 @app.route('/')
@@ -27,6 +31,8 @@ def fetch_jobs():
     job_df = jobs.data_frame
 
     ExportData(job_df)
+    ExportData(job_df, 'csv')
+    ExportData(job_df, 'excel')
 
     # Return the results to the page (could be JSON, or redirect to another page)
     return render_template('result.html')
@@ -40,6 +46,24 @@ def display_json():
 
     # Pass the data to the template
     return render_template('ViewJson.html', data=data)
+
+
+@app.route('/download_json')
+def download_json():
+    # Provide the JSON file for download
+    return send_file(JSON_FILE_PATH, as_attachment=True)
+
+
+@app.route('/download_csv')
+def download_csv():
+    # Provide the CSV file for download
+    return send_file(CSV_FILE_PATH, as_attachment=True)
+
+
+@app.route('/download_excel')
+def download_excel():
+    # Provide the Excel file for download
+    return send_file(EXCEL_FILE_PATH, as_attachment=True)
 
 
 if __name__ == '__main__':
